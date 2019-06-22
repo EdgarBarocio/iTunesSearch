97ccc6a5e0ef7c5061e05ff3b0f8b872e2846349
Edgar Barocio <edgar.barocio@gmail.com>
2019-06-22 16:59:44 -0700

    Base files

---
+++ CodingChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodingChallenge
+//
+//  Created by Edgar Barocio on 6/22/19.
+//  Copyright © 2019 Edgar Barocio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
